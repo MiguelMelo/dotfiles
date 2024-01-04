c9ee3e26b8c0b7a6428db631f060ba5c1bdd85fc
Carlos Silva <miguel21.melo21@gmail.com>
2024-01-03 23:17:44 -0300

    modularize all nvim plugins 😁

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,3 @@
-local opt = vim.opt
-
-opt.expandtab = true
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
-
-opt.autoindent = true
-opt.smartindent = true
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -21,52 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-    }
-  }
-}
-
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-require("catppuccin").setup({
-  flavour = "mocha",
-  color_overrides = {
-    mocha = {
-      base = "#0b0b11",
-      mantle = "#0b0b11",
-      crust = "#0b0b11"
-    } 
-  }
-})
-
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-  ensure_installed = { "lua", "javascript", "typescript" },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-local builtin = require('telescope.builtin')
-
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader> ', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-vim.keymap.set('n', '<leader>e', ':Neotree toggle filesystem reveal left<CR>', {})
-
-vim.cmd.colorscheme "catppuccin"
+require("options")
+require("lazy").setup("plugins")
---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+vim.g.mapleader = ' '
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+
+opt.autoindent = true
+opt.smartindent = true
